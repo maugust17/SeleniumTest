133597ebb3dc588b71a4f799da1ba99e32c1128d
maugust17 <maugust07@gmail.com>
2022-07-27 18:10:04 -0300

    Update Busqueda.java

--- Pruebas/src/test/java/Google/Busqueda.java
+++ Pruebas/src/test/java/Google/Busqueda.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
